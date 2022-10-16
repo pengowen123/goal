bfd4f82b1cedaa00911b124f72af62d559679fb4
Owen Sanchez <pengowen816@gmail.com>
2022-10-16 13:14:58 -0700

    Update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 extern crate toml;
-#[macro_use]
 extern crate clap;
 extern crate app_dirs;
 #[macro_use]
@@ -8,7 +7,7 @@ extern crate tempdir;
 extern crate open;
 
 use toml::Value;
-use clap::{App, AppSettings, Arg, SubCommand};
+use clap::{Parser, Subcommand, crate_version};
 use app_dirs::{AppInfo, AppDataType};
 use std::io::{self, Write, Read};
 use std::fs::{File, OpenOptions};
@@ -23,11 +22,6 @@ const VERSION: &'static str = crate_version!();
 
 const GOAL_FILE: &'static str = "goal.toml";
 const NO_GOAL_MSG: &'static str = "There is no current goal";
-
-const SET_COMMAND: &'static str = "set";
-const REMOVE_COMMAND: &'static str = "remove";
-const SHOW_COMMAND: &'static str = "show";
-const EDIT_COMMAND: &'static str = "edit";
 
 const GOAL_KEY: &'static str = "goal";
 const GOAL_TEXT_KEY: &'static str = "text";
@@ -62,25 +56,29 @@ macro_rules! multiline_string {
 }
 
 fn open_goal_file() -> io::Result<File> {
-    let mut path = app_dirs::app_root(AppDataType::UserData, &APP_INFO).or_else(|e| {
-        Err(io::Error::new(io::ErrorKind::Other, e))
-    })?;
+    let mut path = app_dirs::app_root(AppDataType::UserData, &APP_INFO)
+        .or_else(|e| Err(io::Error::new(io::ErrorKind::Other, e)))?;
 
     path.push(GOAL_FILE);
 
     if !path.exists() {
         let mut file = File::create(path.clone())?;
-        file.write_all(format!(base_goal_file!(), empty_string!(), empty_string!()).as_bytes())?;
+        file.write_all(
+            format!(
+                base_goal_file!(),
+                empty_string!(),
+                empty_string!()
+            ).as_bytes(),
+        )?;
     }
 
-    OpenOptions::new()
-        .write(true)
-        .read(true)
-        .open(path)
+    OpenOptions::new().write(true).read(true).open(path)
 }
 
 fn parse_goal(text: &str) -> Option<Goal> {
-    let parsed = toml::Parser::new(text).parse().expect("Goal file was invalid: failed to parse");
+    let parsed: toml::Value = toml::from_str(text).expect(
+        "Goal file was invalid: failed to parse",
+    );
 
     let goal = inner!(
         parsed.get(&GOAL_KEY.to_string())
@@ -125,27 +123,35 @@ fn set_goal(new_goal: &str, deadline: Option<String>) -> io::Result<()> {
 
     goal_file.set_len(0)?;
 
-    let deadline = deadline.map(|d| multiline_string!(d)).unwrap_or(empty_string!().to_string());
+    let deadline = deadline.map(|d| multiline_string!(d)).unwrap_or(
+        empty_string!()
+            .to_string(),
+    );
 
-    goal_file.write_all(format!(base_goal_file!(),
-                                multiline_string!(new_goal),
-                                deadline)
-            .as_bytes())?;
+    goal_file.write_all(
+        format!(
+            base_goal_file!(),
+            multiline_string!(new_goal),
+            deadline
+        ).as_bytes(),
+    )?;
 
     println!("Goal set.");
 
     Ok(())
 }
 
-fn edit_goal(editor: Option<String>) -> io::Result<()> {
+fn edit_goal(editor: Option<String>, new_deadline: Option<String>) -> io::Result<()> {
     let tmp_dir = tempdir::TempDir::new("goal-edit")?;
     let file_path = tmp_dir.path().join("goal-edit");
     let mut tmp_file = File::create(file_path.clone())?;
 
     // Get current goal (and the deadline for later)
     let current_goal = get_goal()?;
-    let (goal, deadline) = current_goal.map(|g| (g.text, g.deadline))
-        .unwrap_or((String::new(), None));
+    let (goal, deadline) = current_goal.map(|g| (g.text, g.deadline)).unwrap_or((
+        String::new(),
+        None,
+    ));
 
     // Write the current goal to the temp file, will be displayed on editor start
     writeln!(tmp_file, "{}", goal)?;
@@ -159,7 +165,8 @@ fn edit_goal(editor: Option<String>) -> io::Result<()> {
     });
 
     // Open the editor
-    let exit_code = process::Command::new(editor).arg(file_path.clone())
+    let exit_code = process::Command::new(editor)
+        .arg(file_path.clone())
         .spawn()?
         .wait()?;
 
@@ -174,7 +181,7 @@ fn edit_goal(editor: Option<String>) -> io::Result<()> {
     tmp_file.read_to_string(&mut new_goal)?;
 
     // Set the goal
-    set_goal(new_goal.trim(), deadline)?;
+    set_goal(new_goal.trim(), new_deadline.or(deadline))?;
 
     tmp_dir.close()?;
 
@@ -185,16 +192,20 @@ fn remove_goal() -> io::Result<()> {
     let mut goal_file = open_goal_file()?;
 
     goal_file.set_len(0)?;
-    goal_file.write_all(format!(base_goal_file!(), empty_string!(), empty_string!()).as_bytes())
+    goal_file.write_all(
+        format!(base_goal_file!(), empty_string!(), empty_string!()).as_bytes(),
+    )
 }
 
 fn show_current_goal() -> io::Result<()> {
     let goal = get_goal()?;
 
     if let Some(goal) = goal {
-        println!("Current goal: {}\nDeadline: {}",
-                 goal.text,
-                 goal.deadline.unwrap_or("None".to_string()));
+        println!(
+            "Current goal: {}\nDeadline: {}",
+            goal.text,
+            goal.deadline.unwrap_or("None".to_string())
+        );
     } else {
         println!("{}", NO_GOAL_MSG);
     }
@@ -208,62 +219,57 @@ struct Goal {
 }
 
 impl Goal {
-    pub fn new(goal: String, deadline: Option<String>) -> Goal {
+    fn new(text: String, deadline: Option<String>) -> Goal {
         Goal {
-            text: goal,
-            deadline: deadline,
+            text,
+            deadline,
         }
     }
 }
 
+/// The top-level clap CLI type
+#[derive(Parser)]
+#[command(name = NAME)]
+#[command(version = VERSION)]
+#[command(about = "Keeps track of your current goal")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Clone)]
+enum Commands {
+    /// Shows the current goal
+    Show,
+    /// Sets the current goal
+    Set {
+        /// The new goal
+        #[arg(value_name = "new goal")]
+        new_goal: String,
+        /// The deadline for the goal
+        #[arg(short, long)]
+        deadline: Option<String>,
+    },
+    /// Opens the goal in your editor
+    Edit {
+        /// The editor to use
+        #[arg(short, long)]
+        editor: Option<String>,
+        /// The deadline for the goal
+        #[arg(short, long)]
+        deadline: Option<String>,
+    },
+    /// Removes the current goal
+    Remove,
+}
+
 fn main() {
-    let matches = App::new(NAME)
-        .version(VERSION)
-        .about("Keeps track of your current goal")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(SubCommand::with_name(SET_COMMAND)
-            .about("Sets the current goal")
-            .arg(Arg::with_name("new goal")
-                .help("The new goal")
-                .required(true))
-            .arg(Arg::with_name("deadline")
-                .help("The deadline for the goal")
-                .short("d")
-                .long("deadline")
-                .takes_value(true)))
-        .subcommand(SubCommand::with_name(REMOVE_COMMAND).about("Removes the current goal"))
-        .subcommand(SubCommand::with_name(SHOW_COMMAND).about("Shows the current goal"))
-        .subcommand(SubCommand::with_name(EDIT_COMMAND)
-            .about("Opens the goal in your editor")
-            .arg(Arg::with_name("editor")
-                .help("The editor to use")
-                .short("e")
-                .long("editor")
-                .takes_value(true)))
-        .get_matches();
+    let cli = Cli::parse();
 
-    if let Some(matches) = matches.subcommand_matches(SET_COMMAND) {
-        let new_goal = matches.value_of("new goal").unwrap();
-
-        if let Err(e) = set_goal(new_goal,
-                                 matches.value_of("deadline").map(|s| s.to_string())) {
-            error!(e);
-        }
-    }
-
-    if let Some(matches) = matches.subcommand_matches(EDIT_COMMAND) {
-        edit_goal(matches.value_of("editor").map(|s| s.to_string())).unwrap_or_else(|e| error!(e));
-    }
-
-    match matches.subcommand_name() {
-        Some(REMOVE_COMMAND) => {
-            remove_goal().unwrap_or_else(|e| error!(e));
-        }
-        Some(SHOW_COMMAND) => {
-            show_current_goal().unwrap_or_else(|e| error!(e));
-        }
-        Some(EDIT_COMMAND) => {}
-        Some(SET_COMMAND) => {}
-        _ => unreachable!(),
-    }
+    match cli.command {
+        Commands::Show => show_current_goal(),
+        Commands::Set { new_goal, deadline } => set_goal(&new_goal, deadline),
+        Commands::Edit { editor, deadline } => edit_goal(editor, deadline),
+        Commands::Remove => remove_goal(),
+    }.unwrap_or_else(|e| error!(e));
 }
